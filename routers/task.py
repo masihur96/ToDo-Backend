6a09bd5b30e4d5d87d957e82775f8923c111f6c3
masih <engi.masihurrahman@gmail.com>
2023-03-03 11:40:28 +0600

    Push

--- routers/task.py
+++ routers/task.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,Depends,status,HTTPException
 from database import get_db
 from sqlalchemy.orm import Session
 from typing import List
-import models,schemas
+import models,schemas,oauth2
 from repository import task_repo
 
 
@@ -11,28 +11,28 @@ router = APIRouter(
     tags=['Task']
 )
 
-@router.get('',status_code=status.HTTP_200_OK)
-def all_task(db:Session = Depends(get_db)):
+@router.get('/',status_code=status.HTTP_200_OK)
+def all_task(db:Session = Depends(get_db),current_user:schemas.UserModel=Depends(oauth2.get_current_user)):
         return task_repo.all_task(db)
 
 # create Task
-@router.post('',status_code=status.HTTP_201_CREATED)
-def create_task(request:schemas.TaskModel,db:Session = Depends(get_db)):
+@router.post('/',status_code=status.HTTP_201_CREATED)
+def create_task(request:schemas.TaskModel,db:Session = Depends(get_db),current_user:schemas.UserModel=Depends(oauth2.get_current_user)):
     return  task_repo.create_task(request,db)
 
 #Get Task by ID
 @router.get('/{id}',status_code=200,response_model=schemas.ShowTaskModel)
-def task_by_user(id,db:Session = Depends(get_db)):
+def task_by_user(id,db:Session = Depends(get_db),current_user:schemas.UserModel=Depends(oauth2.get_current_user)):
     return task_repo.single_task(id,db)
 
 #Delete  Task
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(id,db:Session = Depends(get_db)):
+def delete_task(id,db:Session = Depends(get_db),current_user:schemas.UserModel=Depends(oauth2.get_current_user)):
     return task_repo.delet_single_task(id,db)
 
 #Update Task
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED,)
-def updated_task(id,request:schemas.TaskModel,db:Session = Depends(get_db)):
+def updated_task(id,request:schemas.TaskModel,db:Session = Depends(get_db),current_user:schemas.UserModel=Depends(oauth2.get_current_user)):
     return task_repo.update_single_task(id,request,db)
 
 
--- routers/user.py
+++ routers/user.py
@@ -12,27 +12,27 @@ router = APIRouter(
 )
 
 # create User
-@router.post('/user',status_code=status.HTTP_201_CREATED,response_model=schemas.ShowUserModel)
+@router.post('/',status_code=status.HTTP_201_CREATED,response_model=schemas.ShowUserModel)
 def create_user(request:schemas.UserModel,db:Session = Depends(get_db)):
     return user_repo.create_task(request,db)
 
 #Get All User
-@router.get('/users',response_model=List[schemas.ShowUserModel],status_code=status.HTTP_200_OK)
+@router.get('/',response_model=List[schemas.ShowUserModel],status_code=status.HTTP_200_OK)
 def all_user(db:Session = Depends(get_db)):
  
         return user_repo.all_user(db)
 
 #Get User by ID
-@router.get('/user/{id}',status_code=200,response_model=schemas.ShowUserModel)
+@router.get('/{id}',status_code=200,response_model=schemas.ShowUserModel)
 def user_by_id(id,db:Session = Depends(get_db)):
     return user_repo.get_single_user(id,db)
 
 #Delete User
-@router.delete('/users/{id}',status_code=status.HTTP_204_NO_CONTENT,)
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT,)
 def delete_user(id,db:Session = Depends(get_db)):
     return user_repo.delete_single_user(id,db)
 
 #Update User
-@router.put('/users/{id}',status_code=status.HTTP_202_ACCEPTED)
+@router.put('/{id}',status_code=status.HTTP_202_ACCEPTED)
 def updated_users(id,request:schemas.ShowUserModel,db:Session = Depends(get_db)):
     return user_repo.update_single_user(id,request,db)
